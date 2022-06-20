b2af66680b248edef5250793478455b848e6340c
Rocky <107879727+Rockyteer@users.noreply.github.com>
2022-06-20 19:03:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+'''
+userName = input("Enter your name: ")
+print( "Hey " + userName)
+
+userGrade= input("What grade are you in? ")
+print("Hey "+ userName + "," +userGrade +" being that grade sucks")
+
+userYear= int(input( "What year were you born? "))
+userAge =2022 - userYear
+print(str(userAge) + " is so young.")
+'''
+
+
+#create your own madlibs
+#Have at least 5 inputs
+
+
+noun= input("Please give me a noun:")
+verb= input("Please give me a verb: ")
+adjective= input("Please give me an adjective: ")
+noun2= input("Please give me another noun: ")
+verb2= input("Please give me another verb: ")
+
+print("The "+ noun + " " + verb +" the now "+ adjective + " " + noun2 + " "+ "who was now " +verb2 + " "+  "after what just happened.")
