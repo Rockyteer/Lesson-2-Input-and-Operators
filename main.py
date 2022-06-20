cd2af2a5b4969d9ffb2570f3aabcf0a432d405a8
Rocky <107879727+Rockyteer@users.noreply.github.com>
2022-06-20 20:10:10 +0000

    Updated

--- main.py
+++ main.py
@@ -16,9 +16,15 @@ print(str(userAge) + " is so young.")
 
 
 noun= input("Please give me a noun:")
-verb= input("Please give me a verb: ")
+verb= input("Please give me a past tense verb: ")
 adjective= input("Please give me an adjective: ")
 noun2= input("Please give me another noun: ")
 verb2= input("Please give me another verb: ")
+
+
+
+
+
+
 
 print("The "+ noun + " " + verb +" the now "+ adjective + " " + noun2 + " "+ "who was now " +verb2 + " "+  "after what just happened.")
